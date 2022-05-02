011484d51a10031c67dfb1ca0e2b6914c58de028
LukasHedegaard <lukasxhedegaard@gmail.com>
2022-05-02 11:37:52 +0000

    Update parse_runs and add kin results

--- parse_runs.py
+++ parse_runs.py
@@ -12,8 +12,8 @@ def read_mAPs(filename: str):
 
 
 def name2vars(filename):
-    pattern = re.compile(r"oadtr-(\w\d)_clspos([-\d]+)_seed(\d)")
-    block, clspos, seed = re.match(pattern, filename).groups()
+    pattern = re.compile(r"oadtr-(\w\d)_clspos([-\d]+)_seed(\d)_([-\w]+).txt")
+    block, clspos, seed, dataset = re.match(pattern, filename).groups()
     clspos = {
         "-1": "-",
         "0": "1A",
@@ -22,29 +22,24 @@ def name2vars(filename):
         "3": "2F",
         "4": "3A",
     }[clspos]
-    return (block, clspos, seed)
+    return (block, clspos, seed, dataset)
 
 
 run_files = (Path(__file__).parent / "runs").glob("*.txt")
 results = []
 
 for f in run_files:
-    block, clspos, seed = name2vars(f.name)
+    block, clspos, seed, dataset = name2vars(f.name)
     mAPs = read_mAPs(f)
-    results.append([block, clspos, seed, mAPs])
+    results.append([block, clspos, seed, dataset, mAPs])
 
 
-print("epoch 1 results:")
-print([[*r[:-1], r[-1][0]] for r in results])
+print("epoch1_data=", [[*r[:-1], r[-1][0]] for r in results])
 
-print("\nepoch 2 results:")
-print([[*r[:-1], r[-1][1]] for r in results])
+print("\nepoch2_data=", [[*r[:-1], r[-1][1]] for r in results])
 
-print("\nepoch 3 results:")
-print([[*r[:-1], r[-1][2]] for r in results])
+print("\nepoch3_data=", [[*r[:-1], r[-1][2]] for r in results])
 
-print("\nepoch 4 results:")
-print([[*r[:-1], r[-1][3]] for r in results])
+print("\nepoch4_data=", [[*r[:-1], r[-1][3]] for r in results])
 
-print("\nbest results:")
-print([[*r[:-1], max(r[-1])] for r in results])
+print("\nepoch_best_data=", [[*r[:-1], max(r[-1])] for r in results])
